d471b4d871aa7087ab5cf5d0fb365797181ccc9f
mateuszao <mateusgarcia@id.uff.br>
2024-12-11 19:15:42 -0300

    Fix modal and icons

--- javascript/map.js
+++ javascript/map.js
@@ -1,5 +1,20 @@
 const hamburger = document.querySelector('.hamburger');
 const header = document.querySelector('.header-bg');
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
+
 
 hamburger.addEventListener('click', () => {
   header.classList.toggle('nav-active');
