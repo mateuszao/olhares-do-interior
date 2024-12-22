f43f9519333ea4c32bec9f13a36f47387ec5ce86
mateuszao <mateusgarcia@id.uff.br>
2024-12-22 08:00:21 -0300

    Fix map and modal

--- javascript/map.js
+++ javascript/map.js
@@ -1,4 +1,8 @@
-const map = L.map("map").setView([-22.4084,  -44.2604], 13);
+const map = L.map('map', {
+  center: [-22.4084, -44.2604],
+  zoom: 13,
+  gestureHandling: true
+});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 80,
@@ -8,13 +12,36 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //Marker add
-var marker = L.marker([-22.4084,  -44.2604])
-.bindPopup('<iframe width="300" height="300" src="https://www.youtube.com/embed/fM9wiB7DR_k?si=PG0haDEaNVFCtSFd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
-.addTo(map);
+L.marker([-22.4084, -44.2604]).addTo(map)
+    .on('click', function() {
+        mostrarModal('Título do Local 1', '<iframe width="300" height="300" src="https://www.youtube.com/embed/fM9wiB7DR_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+});
 
-//Marker add
-var marker = L.marker([-22.1078,  -44.1078])
-.bindPopup(
-  '<h1>Um titulo para o map</h1><p>Texto texto texto texto</p> <iframe width="300" height="300" src="https://www.youtube.com/embed/fM9wiB7DR_k?si=PG0haDEaNVFCtSFd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-)
-.addTo(map);
+// Adicione o segundo marcador com evento de clique
+L.marker([-22.1078, -44.1078]).addTo(map)
+    .on('click', function() {
+        mostrarModal('Um título para o mapa', '<p>Texto texto texto texto</p><iframe width="300" height="300" src="https://www.youtube.com/embed/fM9wiB7DR_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+});
+
+
+function mostrarModal(titulo, conteudo) {
+  // Defina o título e o conteúdo do modal
+  document.getElementById('modal-titulo').innerText = titulo;
+  document.getElementById('modal-conteudo').innerHTML = conteudo;
+
+  // Exiba o modal
+  document.getElementById('fade').classList.remove('hide');
+  document.getElementById('modal').classList.remove('hide');
+}
+
+// Fechar o modal ao clicar no fundo escuro
+document.getElementById('fade').addEventListener('click', function() {
+  document.getElementById('fade').classList.add('hide');
+  document.getElementById('modal').classList.add('hide');
+});
+
+// Fechar o modal ao clicar no botão de fechar
+document.getElementById('modal-fechar').addEventListener('click', function() {
+  document.getElementById('fade').classList.add('hide');
+  document.getElementById('modal').classList.add('hide');
+});
